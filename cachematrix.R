a473b249f97becfb003024dd074ce74d508bdd88
Victor Ribeiro <vribeiro@gmail.com>
2015-09-26 13:29:56 +0200

    More polishing on comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Summary :
-## makeCacheMatrix : creates a special 'matrix' that allows caching of inverse of 'matrix'
-## cacheSolve : get the cache of a 'makeCacheMatrix' or calculates and return the inverse if necessary
+## makeCacheMatrix : creates (and return) a special 'matrix' that allows caching of inverse of 'matrix'
+## cacheSolve : return the inverse of special 'matrix', retrieving it from a cache or calculating it 
+##   for the first time if necessary
+## IMPORTANT : no error handling if matrix does not have an inverse
 
 ## makeCacheMatrix : creates a special 'matrix', which is really a list containing the following functions
 ##  get() = get the value of the matrix
@@ -26,11 +28,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## cacheSolve : returns the inverse of matrix cached in a special list (makeCacheMatrix) OR
 ##    if not cached, calculate and return the inverse of the matrix stored in 'x' 
 ##    IMPORTANT : I've decided for making clear that the parameter for cacheSolve is makeCacheMatrix and
-##    not including "..." as it will not make sense for the assignment, given solve(x, ...) 
-##    will do something different of simply calculating the inverse of a matrix
+##    not including "..." as it will not make sense for the assignment and given solve(x, ...) 
+##    will do something different of simply calculating the inverse of a matrix if additional parameters
+##    are submitted
 cacheSolve <- function(x = makeCacheMatrix()) {
   m <- x$getinverseofmatrix() # get the inverse of the matrix in 'x' 
   
+  # this is slightly different of the makeVector example, as I find this easier and clearer to read
+  # and it eliminates the need of return()
+
   # if m is null, get the matrix, calculate the inverse, cache the result and return the inverse
   if(is.null(m)) {
     data <- x$get()
